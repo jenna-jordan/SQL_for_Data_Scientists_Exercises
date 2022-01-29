8e4344a4ee3cabcf609d9ab8ba984e710e6946b5
Jenna Jordan <jenna.jrdn@gmail.com>
2022-01-29 16:47:32 -0500

    answered 2nd curiosity question

--- Exercises/Chapter_9/exercise_3.sql
+++ Exercises/Chapter_9/exercise_3.sql
@@ -7,14 +7,15 @@
 
 -- Let's take a look at the tables
 SELECT *
-FROM product p;
+FROM product;
 
 SELECT *
-FROM vendor_inventory vi;
+FROM vendor_inventory;
 
 SELECT *
-FROM customer_purchases cp;
+FROM customer_purchases;
 
+-- CURIOSITY QUESTION #1
 -- I wonder how often the original price differs from the sold price 
 
 -- First, let's see if the original price can change depending on the date 
@@ -35,4 +36,23 @@ SELECT DISTINCT cp.vendor_id, cp.product_id, cp.customer_id,
 FROM vendor_inventory vi 
 	JOIN customer_purchases cp ON vi.vendor_id = cp.vendor_id AND vi.product_id = cp.product_id
 WHERE vi.original_price != cp.cost_to_customer_per_qty;
+
+-- CURIOSITY QUESTION #2
+
+-- from @Matty_TwoShoes on Twitch:
+-- it looks like sales and products from a farmers market. 
+-- i would be curious if purchases include more than 1 item or not and if they do, 
+-- are the multi-item purchases cheap or expensive items?
+
+-- Can we identify multi-item purchases based on the transaction_time
+
+SELECT pc.*, p.product_name 
+FROM(
+	SELECT *, COUNT(product_id) OVER 
+		(PARTITION BY market_date, transaction_time, vendor_id, customer_id 
+		ORDER BY market_date, transaction_time, vendor_id, customer_id) as product_count_per_purchase
+	FROM customer_purchases cp
+) pc JOIN product p ON pc.product_id = p.product_id
+WHERE pc.product_count_per_purchase > 1
+ORDER BY market_date, transaction_time, vendor_id, customer_id;
 
